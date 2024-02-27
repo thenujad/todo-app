c8747657ffe5e40f2a11e31d352fd48c7722f5d3
thenujad <dulwanat@gmail.com>
2024-02-27 21:05:05 +0530

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,22 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    
+    {
+        id: 1,
+        title: "Like this video",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Subscribe to Coding in Flow",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Leave a nice comment",
+        completed: false,
+    },
+
+];
+
