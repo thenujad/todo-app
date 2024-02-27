7b08113becf049d907b8f95737e7b2f78d3bedee
thenujad <dulwanat@gmail.com>
2024-02-27 22:07:23 +0530

    New Updated

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <h1 className="font-bold text-3xl text-center">My Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo} />
         <TodoList
--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -22,11 +22,11 @@ export default function AddTodoForm({ onSubmit }: AddTodoFormProps) {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="What needs to be done?"
-          className="rounded-5-md grow border border-gray-400 p-2"
+          className="rounded-5-md grow border border-emerald-400 p-2"
           />
           <button 
            type="submit"
-           className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+           className="w-16 rounded-e-md bg-orange-950 text-white hover:bg-slate-800"
            >
             Add
           </button>
